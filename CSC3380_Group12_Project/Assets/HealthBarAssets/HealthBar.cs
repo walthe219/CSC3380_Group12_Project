d72da34f50a7e2b4fee80a1808cc448abb4004ee
vanikmakaryan <vanikmakaryan02@gmail.com>
2025-09-28 19:39:46 -0500

    HealthBar
    HealthBar made
    HealthBar has script with maxHealth etc.
    //Will work on dmg with Mikey

---
+++ CSC3380_Group12_Project/Assets/HealthBarAssets/HealthBar.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void setMaxHealth(int health){
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void setHealth(int health){
+            slider.value = health;
+    }
+   
+}
