a8baca585fa51bbc93c80714a29d9a5bdee7b598
vmakaa <vanikmakaryan02@gmail.com>
2025-10-14 21:40:42 -0500

    Apply Simple Stats + Event stats apply
    Implemented a apply simple stats function and planned out how to create an apply upgrade delta stats function using the event system invoke

--- CSC3380_Group12_Project/Assets/Scripts/SystemScripts/PlayerStats.cs
+++ CSC3380_Group12_Project/Assets/Scripts/SystemScripts/PlayerStats.cs
@@ -13,6 +13,40 @@ public class PlayerStats : MonoBehaviour
     public int currentStamina;
     public int currentMagSize;
     public int currentAmmo;
+
+    public enum upgradeTypesToApply {HealthUp, AmmoUp, MagUp, StamUp}
+
+    public UpgradeData1 HealthUp;
+    public UpgradeData1 AmmoUp;
+    public UpgradeData1 MagUp;
+    public UpgradeData1 StamUp;
+
+    public void applyUpgradeSimpleStats(upgradeTypesToApply upgrade){
+        switch(upgrade){
+            case upgradeTypesToApply.HealthUp:
+                currentHealth += HealthUp.deltaHealth;
+                break;
+            case upgradeTypesToApply.StamUp:
+                currentStamina += StamUp.deltaStamina;
+                break;
+            case upgradeTypesToApply.MagUp:
+                currentMagSize += MagUp.deltaMag;
+                break;
+            case upgradeTypesToApply.AmmoUp:
+                currentAmmo += AmmoUp.deltaAmmo;
+                break;
+            default:
+                Debug.LogWarning(upgrade + " is not a valid upgrade.");
+                break;
+
+        }
+    }
+
+    public void applyEventUpgrdDeltaVal(){
+        //Put this into a event that will invoke all functions associated to an upgrade depending on what upgrade it is
+            //You can ivoke the upgradetype and the deltaval and then playerstats will process what stat accords to the upgrade and add the deltaval to it
+
+    }
     
 
     public void Start(){
@@ -21,11 +55,11 @@ public class PlayerStats : MonoBehaviour
     }
 
     private void OnEnable(){
-        //UnlockScript.unlockD += PlayerStatUpgrdMngr;
+        
     }
 
     private void OnDisable(){
-        //UnlockScript.unlockD -= PlayerStatUpgrdMngr;
+       
     }
 
     private void PlayerStatUpgrdMngr(string upgrade){
