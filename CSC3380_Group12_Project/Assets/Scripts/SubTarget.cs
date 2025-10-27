8289087bd07a81dde08aa6d4bcbc9b87c3cdfe3e
JosiahMatriarch <corbinbrescher@gmail.com>
2025-10-26 20:58:05 -0500

    save

---
+++ CSC3380_Group12_Project/Assets/Scripts/SubTarget.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SubTarget : MonoBehaviour
+{
+    public Target target;
+    public float dmgMult = 1f;
+    public float limbHealth = 50f;
+
+    public void TakeDamage(float damage)
+    {
+        float totalDmg = damage * dmgMult;
+
+        if (limbHealth <= 0f)
+        {
+            Debug.Log("The bitch is crippled!");
+            Cripple(totalDmg);
+        } 
+        else
+        {
+            limbHealth -= totalDmg;
+            target.TakeDamage(totalDmg, transform.name);
+        }
+
+    }
+
+    void Cripple(float damage)
+    {
+        target.TakeDamage(damage*0.75f, transform.name);
+    }
+}
