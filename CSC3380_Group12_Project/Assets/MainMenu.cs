02e6ba1da04a3b89ffd42c2924e9044e2649ae64
vmakaa <124320161+vmakaa@users.noreply.github.com>
2025-10-31 10:45:42 -0500

    Update MainMenu.cs

--- CSC3380_Group12_Project/Assets/MainMenu.cs
+++ CSC3380_Group12_Project/Assets/MainMenu.cs
@@ -7,16 +7,16 @@ public class MainMenu : MonoBehaviour
 public GameObject MainMenuUI;
 
     public void PlayGame(){
-        //MainMenuUI.SetActive(false);
+        //MainMenuUI.SetActive(false); < ignore
         SceneManager.LoadScene("TestScene", LoadSceneMode.Single);
 
-        //SceneManager.LoadScene("UI_Scene", LoadSceneMode.Additive);
+        //SceneManager.LoadScene("UI_Scene", LoadSceneMode.Additive); < ignore
         
 
     }
 
     public void QuitGame(){
-        Debug.Log("Quitting Game");
+        //Debug.Log("Quitting Game");
         Application.Quit();
     }
     
