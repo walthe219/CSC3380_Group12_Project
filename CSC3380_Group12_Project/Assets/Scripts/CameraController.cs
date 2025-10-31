9a34c954d710f7eeec26cb98dda570b3a1e7eab7
Michael Jennings <mjennings0033@gmail.com>
2025-10-30 22:45:00 -0500

    Fixed Camera bugs
    Fixed:
    - Camera not freezing during pause menu
    - Camera sensitivity slider now works with new camera system
    - Sensitive slider adjusted a bit

--- CSC3380_Group12_Project/Assets/Scripts/CameraController.cs
+++ CSC3380_Group12_Project/Assets/Scripts/CameraController.cs
@@ -4,13 +4,13 @@ using UnityEngine.InputSystem;
 public class CameraController : MonoBehaviour
 {
 
-    public float minX = -60f;
-    public float maxX = 60f;
+    private float minX = -90f;
+    private float maxX = 90f;
 
     public float sensitivity;
-    public Camera cam;
+    public Transform orientation;
     public InputAction look;
-
+    
     float rotY = 0f;
     float rotX = 0f;
 
@@ -27,8 +27,12 @@ public class CameraController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        sensitivity = 60f;
+        // Hides + Locks Cursor to center of screen
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        // Inputs
         if(InputSystem.actions)
         {
             look = InputSystem.actions.FindAction("Player/Look");
@@ -39,13 +43,21 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        rotY += look.ReadValue<Vector2>().x * sensitivity;
-        rotX += look.ReadValue<Vector2>().y * sensitivity;
+        // Gets mouse input
+        rotY += look.ReadValue<Vector2>().x * Time.deltaTime * sensitivity;
+        rotX += -look.ReadValue<Vector2>().y * Time.deltaTime * sensitivity;
 
         rotX = Mathf.Clamp(rotX, minX, maxX);
 
-        transform.localEulerAngles = new Vector3(0, rotY, 0);
-        cam.transform.localEulerAngles = new Vector3(-rotX, 0, 0);
+        //transform.localEulerAngles = new Vector3(0, rotY, 0);
+        //cam.transform.localEulerAngles = new Vector3(-rotX, 0, 0);
+        transform.rotation = Quaternion.Euler(rotX, rotY, 0);
+        orientation.rotation = Quaternion.Euler(0, rotY, 0);
+    }
+    public void SetMouseSensitivity(float NewMouseSensitivity)
+    {
+        sensitivity = NewMouseSensitivity;
+        Debug.Log("Sensitivity set to: " + sensitivity);
+
     }
 }
