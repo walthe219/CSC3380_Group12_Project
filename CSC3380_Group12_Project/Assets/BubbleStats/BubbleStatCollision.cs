fdead2ae4440bf67b7fa67853f9c7020da4b01b5
vmakaa <vanikmakaryan02@gmail.com>
2025-11-04 17:35:47 -0600

    Saved scene and script organization tweaks
    Saved the scene, edited some coments, and deleted the incorrect HealthBubble Prefab

--- CSC3380_Group12_Project/Assets/BubbleStats/BubbleStatCollision.cs
+++ CSC3380_Group12_Project/Assets/BubbleStats/BubbleStatCollision.cs
@@ -6,24 +6,20 @@ public class BubbleStatCollision : MonoBehaviour
 {
 
     private void OnTriggerEnter(Collider other){
-        BubbleStatsInventory bubbleinv = other.GetComponent<BubbleStatsInventory>();
+        BubbleStatsInventory bubbleinv = other.GetComponent<BubbleStatsInventory>();//other refers to the player
 
-        // Try to get BubbleType on the object the collider is attached to
-    BubbleType bubble = GetComponent<BubbleType>();
+        //find bubbletype component
+        BubbleType bubble = GetComponent<BubbleType>();
 
-
-    // If we found it, check the type
-    if(bubble != null){//test to see if i can check the type of bubble and tested it by changing the bubble type to mana and it works as intended
-        if(bubble.bubbleType == BubbleType.Type.Health){
-            Debug.Log("Health Bubble collected!");
-            // Add health here
+        // if found check type
+        if(bubble != null){//test to see if i can check the type of bubble and tested it by changing the bubble type to mana and it works as intended
+            if(bubble.bubbleType == BubbleType.Type.Health){
+                Debug.Log("Health Bubble collected!");
+                // Add health here
+            }
         }
-    }
 
         if(bubbleinv != null){
-
-            
-
             Debug.Log("Triggered");
             bubbleinv.BubbleStatCollected();
             gameObject.SetActive(false);
