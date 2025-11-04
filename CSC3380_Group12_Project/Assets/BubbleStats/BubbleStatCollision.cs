e54a71948377d26a6db7d591dc1b40fed3727b48
vmakaa <vanikmakaryan02@gmail.com>
2025-11-04 17:31:12 -0600

    BubbleStatCollision now checks the bubble type
    bubble type is the type of stat boost

--- CSC3380_Group12_Project/Assets/BubbleStats/BubbleStatCollision.cs
+++ CSC3380_Group12_Project/Assets/BubbleStats/BubbleStatCollision.cs
@@ -8,7 +8,22 @@ public class BubbleStatCollision : MonoBehaviour
     private void OnTriggerEnter(Collider other){
         BubbleStatsInventory bubbleinv = other.GetComponent<BubbleStatsInventory>();
 
+        // Try to get BubbleType on the object the collider is attached to
+    BubbleType bubble = GetComponent<BubbleType>();
+
+
+    // If we found it, check the type
+    if(bubble != null){//test to see if i can check the type of bubble and tested it by changing the bubble type to mana and it works as intended
+        if(bubble.bubbleType == BubbleType.Type.Health){
+            Debug.Log("Health Bubble collected!");
+            // Add health here
+        }
+    }
+
         if(bubbleinv != null){
+
+            
+
             Debug.Log("Triggered");
             bubbleinv.BubbleStatCollected();
             gameObject.SetActive(false);
@@ -16,3 +31,4 @@ public class BubbleStatCollision : MonoBehaviour
     }
 
 }
+
---
+++ CSC3380_Group12_Project/Assets/BubbleType.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class BubbleType : MonoBehaviour
+{
+    public enum Type { Health, Mana, Score }
+    public Type bubbleType;
+}
