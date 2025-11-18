3c48f2745fc5a24b0bd0fafb505b46c6896926d5
vmakaa <vanikmakaryan02@gmail.com>
2025-11-17 22:01:51 -0600

    Got the HSM to display random upgrades
    Buy button correctly corresponds to each upgrade too
    
    trying to figure out how to applyupgrade using unity event but mighjt try out differtn approach

--- CSC3380_Group12_Project/Assets/HUD_Assets/HealthSacrifice Assets/HS_Pillar Prefab Materials/PillarScript.cs
+++ CSC3380_Group12_Project/Assets/HUD_Assets/HealthSacrifice Assets/HS_Pillar Prefab Materials/PillarScript.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
 using System;
 using UnityEngine.Events;
+using TMPro;
+using System.Collections;
 
 public class PillarScript : MonoBehaviour
 {
     public Interact OpenFromInteraction;
+    [SerializeField] public TextMeshProUGUI hsubox1;
+    [SerializeField] public TextMeshProUGUI hsubox2;
+    [SerializeField] public TextMeshProUGUI hsubox3;
     [SerializeField] GameObject HealthSacrificeMenu;
+    [SerializeField] PlayerStats CurrentPlayerStats;
     private bool PillarMenuOpened;
+    SacrificeUpgradeData[] upgradechoices;
     public event Action<UpgradeData> UpgradePurchased; // instead of using action use unity event, and then subscribe UpgradePurchased to UpgradeManager.addUpgrade
+
 
     UpgradeSpace sacrificeSpace; //Create UpgradeSpace for Health Sacrifice
     
 
     public void Start(){
         sacrificeSpace = new UpgradeSpace(null, "UpgradeData/HealthSacrifice"); //Note: should work after i merge with josh's branch but wait until further upgradespace testing
-        
+        DisplaySacrificeUpgrades();
         
         
     }
@@ -53,25 +61,28 @@ public class PillarScript : MonoBehaviour
     public void PurchaseUpgrade(int i){
         //upgrade = upgradechoices[i]
         //currentstats.hp * upgrade.healthcostpercentage
+        Debug.Log("Health Cost Percentage: " + upgradechoices[i].HealthCostPercent);
+        CurrentPlayerStats.health = CurrentPlayerStats.health * (1 - (upgradechoices[i].HealthCostPercent/100f));
+        Debug.Log("Health is now: " + CurrentPlayerStats.health);
+        
         
 
     }
 
     public void DisplaySacrificeUpgrades(){
         UpgradeData[] sample = sacrificeSpace.samplePossibleUpgrades(3);
-        SacrificeUpgradeData[] upgradechoices = Array.ConvertAll(sample, element => (SacrificeUpgradeData) element);
+        upgradechoices = Array.ConvertAll(sample, element => (SacrificeUpgradeData) element);
+
+        hsubox1.text = upgradechoices[0].printDescription(ID:true, label:false, stats:true, descr:true);
+        hsubox2.text = upgradechoices[1].printDescription(ID:true, label:false, stats:true, descr:true);
+        hsubox3.text = upgradechoices[2].printDescription(ID:true, label:false, stats:true, descr:true);
+
         //'refresh' method
         //new sample of three upgrades and display the upgrade information in the menu
         //subscribe to event in roomgenerator where upon room completion get a new batch of sacrifice upgrades
         //display upgradechoices[0->2]
         //for each tmp i can do upgradechoice[i].printDescription(parameters blah blah blah)
-         
-        //add i do not say blah blah blah feature
-        //add sex feature
-        //add rizz feature
-        //add mini minecraft feature
-        //add all of overwatch
-        //add 
+
     }
 
 }
