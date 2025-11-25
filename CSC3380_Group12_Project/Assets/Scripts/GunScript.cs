8030e764504eccb62f6feafb545decd2027fa2ac
vmakaa <124320161+vmakaa@users.noreply.github.com>
2025-11-25 15:51:29 -0600

    gunRnage and damage
    Made gunrange and damage read from current player stats

--- CSC3380_Group12_Project/Assets/Scripts/GunScript.cs
+++ CSC3380_Group12_Project/Assets/Scripts/GunScript.cs
@@ -6,8 +6,8 @@ using System.Collections;
 
 public class GunScript : MonoBehaviour
 {
-    public float damage = 10f;
-    public float range = 100f;
+    public float damage;
+    public float range;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
@@ -35,7 +35,8 @@ public class GunScript : MonoBehaviour
         currPlayerStats.reloadSpeed = BasePlayerStats.reloadSpeed;
         currPlayerStats.Firerate = BasePlayerStats.Firerate;
         currPlayerStats.damage = BasePlayerStats.damage;
-
+        damage = currPlayerStats.damage; //Damage for takedamage function since weird behavior is encountered when just currplayerstats.blah is inpuit as parameter
+        range = currPlayerStats.gunRange; //^^ same here
     }
 
     private void OnEnable()
@@ -45,10 +46,23 @@ public class GunScript : MonoBehaviour
 
     void Update()
     {
+
+        if (currPlayerStats.damage != damage) { //If dmaage is not equal to current damage update damage
+            damage = currPlayerStats.damage;
+        }
+
+        if (currPlayerStats.gunRange != range) { //If gunrange is not equal to current gunrange update gunrange
+            range = currPlayerStats.gunRange;
+        }
+
+        //NOTE: gunrange and damage work as intended as of 11/25/25
+
         if (fireAction.WasPressedThisFrame() && currPlayerStats.ammo > 0 && !PauseMenu1.GameIsPaused && Time.timeScale > 0 && !isReloading)
         {
+
             if (currPlayerStats.Firerate <= 0f) //If statement checks if cooldown has reached 0
             {
+                
                 Shoot();
                 currPlayerStats.ammo--;
                 currPlayerStats.Firerate = BasePlayerStats.Firerate; //reset the current cooldown to the gun's cooldown
@@ -67,6 +81,10 @@ public class GunScript : MonoBehaviour
                 StartCoroutine(Reload());
             }
         }
+
+        //Stat Updater Section
+        //Since parameters dont like to take in "currplayerstats.blah" I am updating these values every frame
+        
     }
 
     IEnumerator Reload()
