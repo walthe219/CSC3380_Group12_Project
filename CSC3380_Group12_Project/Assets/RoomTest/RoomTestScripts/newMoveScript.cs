cd4355bfa70f4dbb8612d4c3a4b997b6991de770
JosiahMatriarch <corbinbrescher@gmail.com>
2025-11-30 17:51:02 -0600

    event changes

--- CSC3380_Group12_Project/Assets/RoomTest/RoomTestScripts/newMoveScript.cs
+++ CSC3380_Group12_Project/Assets/RoomTest/RoomTestScripts/newMoveScript.cs
@@ -25,17 +25,6 @@ public class newMoveScript : MonoBehaviour
         controller = GetComponent<CharacterController>();
         move = InputSystem.actions.FindAction("Move");
         jump = InputSystem.actions.FindAction("Jump");
-        ControlScriptReference.ScriptsDisabled += Disable;
-        ControlScriptReference.ScriptsEnabled += Enable;
-    }
-
-    void Enable()
-    {
-        this.enabled = true;
-    }
-    void Disable()
-    {
-        this.enabled = false;
     }
 
 
